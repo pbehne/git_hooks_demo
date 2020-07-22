1918d9b0284f22841b59b0f0b67aa7ef46d6104a
Patrick Behne <trickbehne@gmail.com>
2020-07-22 15:09:57 -0500

    Added test_result variable.

--- tests/pre-commit.sh
+++ tests/pre-commit.sh
@@ -2,9 +2,10 @@
 
 echo "Running pre-commit hook"
 ./tests/run-tests
+test_result=$?  # Exit code from last command
 
 # $? stores exit value of the last command
-if [ $? -ne 0 ]; then
+if [ $test_result -ne 0 ]; then
  echo "Tests must pass before commit!"
  exit 1
 fi
