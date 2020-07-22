3ed86f719d75d25ca91647945fdfec95d1147be6
Patrick Behne <trickbehne@gmail.com>
2020-07-22 15:41:52 -0500

    Added stashing.

--- tests/pre-commit.sh
+++ tests/pre-commit.sh
@@ -1,11 +1,34 @@
 #!/usr/bin/env bash
 
 echo "Running pre-commit hook"
-./tests/run-tests
-test_result=$?  # Exit code from last command
+
+# Define a cleanup function                                                     
+function cleanup()                                                              
+{                                                                               
+        echo "Skipping remaining tests"                                         
+        test_result=1  # Make test fail                                         
+}                                                                               
+                                                                                
+# Define a test function                                                        
+function run_tests()                                                            
+{                                                                               
+        ./tests/run-tests                                
+        test_result=$?  # Exit code from tests                                  
+} 
+
+# Stash unstaged changes so they don't affect the tests
+git stash push -q --keep-index
+
+# In case of ctrl+c during testing, run cleanup function
+trap cleanup SIGINT
+# Run pre-commit tests
+run_tests
+
+# Reapply stashed changes
+git stash pop -q
 
 # $? stores exit value of the last command
-if [ $test_result -ne 0 ]; then
+if [[ $test_result -ne 0 ]]; then
  echo "Tests must pass before commit!"
  exit 1
 fi
