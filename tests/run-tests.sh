375a3dd83290566d5e4cb3adfa5350704afbab78
Patrick Behne <trickbehne@gmail.com>
2020-07-22 06:54:37 -0500

    comment

--- tests/run-tests.sh
+++ tests/run-tests.sh
@@ -5,7 +5,7 @@ set -e
 
 # magic line to ensure that we're always inside the root of our application,
 # no matter from which directory we'll run script
-# thanks to it we can just enter `./scripts/run-tests.bash`
+# thanks to it we can just enter `./scripts/run-tests.sh`
 cd "${0%/*}/.."
 
 # let's fake failing test for now 
